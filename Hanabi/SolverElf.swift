030c862445f06810a3845b2d6d9642b20c86b0d5
Geoffrey Hom <geoffhom@gmail.com>
2014-08-06 03:39:55 -0700

    0.0.4 (build 1) • User can walk through through a game.   • User enters a seed value; taps start   • Deck is reproducibly shuffled (in progress)     • Deck made. Next • Shuffle deck.

--- Hanabi/SolverElf.swift
+++ Hanabi/SolverElf.swift
@@ -15,9 +15,38 @@ protocol SolverElfDelegate {
 private var myContext = 0
 
 class SolverElf: NSObject {
-    enum Mode: Int {
-        case Planning, Solving
+    struct Card {
+        var color: Color
+        var numberInt: Int!
     }
+    // Card colors.
+    enum Color: Int {
+        case Blue = 1, Green, Red, White, Yellow
+        func letterString() -> String {
+            var aLetterString: String
+            switch self {
+            case Blue:
+                aLetterString = "B"
+            case Green:
+                aLetterString = "G"
+            case Red:
+                aLetterString = "R"
+            case White:
+                aLetterString = "W"
+            case Yellow:
+                aLetterString = "Y"
+            default:
+                aLetterString = "M"
+            }
+            return aLetterString
+        }
+    }
+    enum Mode: Int {
+        case Planning, Solving, Solved
+    }
+    // Ordered set of Hanabi cards.
+    // so this is an array of Hanabi cards; can be empty
+    var deckCardArray: [Card] = []
     var delegate: SolverElfDelegate? = nil
     var mode: Mode = Mode.Planning {
     didSet {
@@ -30,14 +59,67 @@ class SolverElf: NSObject {
     var numberOfGamesToPlayInt = 1
     var numberOfGamesWonInt = 0
     var numberOfSecondsSpentFloat = 0.0
+    // Number for srandom().
+    var seedNumberOptionalInt: Int? = nil
     override init() {
         super.init()
+    }
+    // Make a starting deck, in order.
+    func makeDeck() {
+        // For each color, add 3/2/2/2/1.
+        self.deckCardArray = []
+        var aCard: Card
+        for anInt in 1...5 {
+            if let anOptionalColor = Color.fromRaw(anInt) {
+                for _ in 1...3 {
+                    aCard = Card(color: anOptionalColor, numberInt: 1)
+                    self.deckCardArray.append(aCard)
+                }
+                for _ in 1...2 {
+                    aCard = Card(color: anOptionalColor, numberInt: 2)
+                    self.deckCardArray.append(aCard)
+                }
+                for _ in 1...2 {
+                    aCard = Card(color: anOptionalColor, numberInt: 3)
+                    self.deckCardArray.append(aCard)
+                }
+                for _ in 1...2 {
+                    aCard = Card(color: anOptionalColor, numberInt: 4)
+                    self.deckCardArray.append(aCard)
+                }
+                aCard = Card(color: anOptionalColor, numberInt: 5)
+                self.deckCardArray.append(aCard)
+            }
+        }
+    }
+    // Print cards, in order, to the console.
+    func printDeck() {
+        print("Deck:")
+        for aCard in self.deckCardArray {
+            print(" \(aCard.color.letterString())\(aCard.numberInt)")
+        }
+        print("\n")
     }
     // User sees results of solving.
     func showResults() {
         println("Games: \(self.numberOfGamesPlayedInt)")
         println("Games won: \(self.numberOfGamesWonInt)")
         println("Time spent: \(self.numberOfSecondsSpentFloat) seconds")
+    }
+    // Pseudo-randomly shuffle the deck.
+    func shuffleDeck() {
+        // how does one shuffle a deck via code; this is probably on google
+        // probably have an array of cards
+        self.makeDeck()
+        println("shuffle deck now")
+        // pull randomly to form a new deck
+        // remember want pseudo-random (reproducible)
+        // just log deck to check
+        self.printDeck()
+    }
+    // Solve a random game.
+    func solveAGame() {
+        self.shuffleDeck()
     }
     func solveGames() {
         self.mode = Mode.Solving
--- Hanabi/WalkThroughGameViewController.swift
+++ Hanabi/WalkThroughGameViewController.swift
@@ -1,16 +1,16 @@
 //
-//  SolveGamesViewController.swift
+//  WalkThroughGameViewController.swift
 //  Hanabi
 //
-//  Created by Geoff Hom on 8/4/14.
+//  Created by Geoff Hom on 8/6/14.
 //  Copyright (c) 2014 Geoff Hom. All rights reserved.
 //
 
 import UIKit
 
-class SolveGamesViewController: UIViewController, SolverElfDelegate, UITextFieldDelegate {
+class WalkThroughGameViewController: UIViewController, SolverElfDelegate, UITextFieldDelegate {
     @IBOutlet weak var cancelButton: UIButton!
-    @IBOutlet weak var numberOfGamesTextField: UITextField!
+    @IBOutlet weak var seedNumberTextField: UITextField!
     var solverElf: SolverElf!
     @IBOutlet weak var startButton: UIButton!
     var viewControllerElf: ViewControllerElf!
@@ -23,22 +23,33 @@ class SolveGamesViewController: UIViewController, SolverElfDelegate, UITextField
     }
     // Play/solve the requested number of games.
     @IBAction func handleStartButtonTapped() {
-        self.solverElf.solveGames()
+        self.solverElf.solveAGame()
     }
     // User interacts with UI. She hears a sound to (subconsciously) know she did something.
     @IBAction func playButtonDownSound() {
         self.viewControllerElf.playButtonDownSound()
     }
-    // Text field is for number of games to play/solve.
+    // Text fields: random-number seed.
+    // multiple text fields?
     // If a valid value, then update model. Show current value in text field.
     func textFieldDidEndEditing(theTextField: UITextField!) {
-        // Valid: Int >= 1.
+        // Valid seed: User should enter either an Int >= 0, or nothing ("") to let computer choose a random seed. If neither, do nothing.
         if let theInt = theTextField.text.toInt() {
-            if theInt >= 1 {
-                self.solverElf.numberOfGamesToPlayInt = theInt
+            if theInt >= 0 {
+                self.solverElf.seedNumberOptionalInt = theInt
             }
         }
-        theTextField.text = String(self.solverElf.numberOfGamesToPlayInt)
+        if theTextField.text == "" {
+            self.solverElf.seedNumberOptionalInt = nil
+        }
+        // Show seed or placeholder.
+        var aString: String
+        if let theInt = self.solverElf.seedNumberOptionalInt {
+            aString = String(theInt)
+        } else {
+            aString = ""
+        }
+        theTextField.text = aString
     }
     // Dismiss keyboard.
     func textFieldShouldReturn(theTextField: UITextField!) -> Bool {
@@ -49,15 +60,15 @@ class SolveGamesViewController: UIViewController, SolverElfDelegate, UITextField
         switch self.solverElf.mode {
         case SolverElf.Mode.Planning:
             self.cancelButton.enabled = false
-            self.numberOfGamesTextField.enabled = true
+            self.seedNumberTextField.enabled = true
             self.startButton.enabled = true
         case SolverElf.Mode.Solving:
             self.cancelButton.enabled = true
-            self.numberOfGamesTextField.enabled = false
+            self.seedNumberTextField.enabled = false
             self.startButton.enabled = false
-        default:
+        case SolverElf.Mode.Solved:
             self.cancelButton.enabled = false
-            self.numberOfGamesTextField.enabled = true
+            self.seedNumberTextField.enabled = false
             self.startButton.enabled = true
         }
     }
